4114ac8ed1426ef89ce4f5d2002d6685b34e962b
harrychan19 <shihai775532>
2019-01-27 14:12:04 +0800

    Initial commit

---
+++ src/main/java/com/cloudlearn/configclient/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.cloudlearn.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+
+}
+
